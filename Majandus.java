662eaf2081a8db111143168934fe8ff02ff2142b
Anett Luigemaa <116277036+anettvist@users.noreply.github.com>
2023-03-29 18:14:37 +0300

    Add files via upload

---
+++ Majandus.java
@@ -0,0 +1,39 @@
+public class Majandus extends Kuldvillak{
+    public Majandus(String küsimus100, String küsimus200, String küsimus300, String küsimus400, String küsimus500, String vastus100, String vastus200, String vastus300, String vastus400, String vastus500) {
+        super(küsimus100, küsimus200, küsimus300, küsimus400, küsimus500, vastus100, vastus200, vastus300, vastus400, vastus500);
+    }
+    @Override
+    public String annaküsimus(int nr) {
+        switch (nr){
+            case 100:
+                return getKüsimus100();
+            case 200:
+                return getKüsimus200();
+            case 300:
+                return getKüsimus300();
+            case 400:
+                return getKüsimus400();
+            case 500:
+                return getKüsimus500();
+        }
+        return "sa ei valinud küsimust";
+    }
+
+    @Override
+    public String annavastus(int nr) {
+        switch (nr){
+            case 100:
+                return getVastus100();
+            case 200:
+                return getVastus200();
+            case 300:
+                return getVastus300();
+            case 400:
+                return getVastus400();
+            case 500:
+                return getVastus500();
+
+        }
+        return "sa ei valinud küsimust";
+    }
+}
